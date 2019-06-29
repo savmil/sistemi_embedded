c790178ff97780e38358de99ff65499e54774d64
Michele Bevilacqua <michele.bevilacqua94@gmail.com>
2019-06-29 12:38:47 +0200

    Aggiornata documentazione esercizio02 GPIO, puliti codici kernek modee
    Manca da pushare e testare il driver UIO di UART

--- Michele/FPGA/GPIO_interrupts/Driver/Kernel Mode/GPIO_kernel_main.c
+++ Michele/FPGA/GPIO_interrupts/Driver/Kernel Mode/GPIO_kernel_main.c
@@ -122,7 +122,7 @@ static int GPIO_probe(struct platform_device *pdev) {
 		}
 	}
 
-	/* Allocazione dell'oggetto GPIO */
+/** Allocazione dell'oggetto GPIO */
 	if ((GPIO_ptr = kmalloc(sizeof(GPIO), GFP_KERNEL)) == NULL) {
 		printk(KERN_ERR "%s: kmalloc ha restituito NULL\n", __func__);
 		return -ENOMEM;
@@ -289,17 +289,17 @@ static irqreturn_t GPIO_irq_handler(int irq, struct pt_regs * regs) {
 		return IRQ_NONE;
 	}
 	
-// Disabilitazione delle interruzioni della periferica
+/** Disabilitazione delle interruzioni della periferica */
 
 	GPIO_GlobalInterruptDisable(GPIO_dev_ptr);
 	GPIO_PinInterruptDisable(GPIO_dev_ptr, GPIO_dev_ptr->irq_mask);
 
-// Setting del valore del flag "can_read"
+/** Setting del valore del flag "can_read" */
 
 	GPIO_SetCanRead(GPIO_dev_ptr);
 	printk(KERN_INFO "funzione %s | valore can_read: %d\n", __func__, GPIO_dev_ptr->can_read);
 
-// Risveglio dei processi sleeping
+/** Risveglio dei processi sleeping */
 
 	GPIO_PinInterruptAck(GPIO_dev_ptr, GPIO_dev_ptr->irq_mask);
 	GPIO_WakeUp(GPIO_dev_ptr);
@@ -330,20 +330,25 @@ static ssize_t GPIO_read (struct file *file_ptr, char *buf, size_t count, loff_t
 	if ((file_ptr->f_flags & O_NONBLOCK) == 0) {
 		printk(KERN_INFO "%s è bloccante\n", __func__);
 
-//Test della variabile "can_read", se non sono state rilevate iterruzioni e il flag O_NONBLOCK non è stato specificato il processo si mette il sleep
+/** Test della variabile "can_read", se non sono state rilevate iterruzioni e il flag O_NONBLOCK non è stato specificato il processo si mette il sleep */
+		
 		GPIO_TestCanReadAndSleep(GPIO_dev_ptr);
-//Il processo è risvegliato dall'arrivo di un'interruzione	
+		
+/** Il processo è risvegliato dall'arrivo di un'interruzione	*/
+		
 		GPIO_ResetCanRead(GPIO_dev_ptr);
 	}
 	else {
 		printk(KERN_INFO "%s non è bloccante\n", __func__);
 	}
-//Accesso ai registri del device
+/** Accesso ai registri del device */
+	
 	read_addr = GPIO_GetDeviceAddress(GPIO_dev_ptr)+*off;
 	data_readed = ioread32(read_addr);
 	printk(KERN_INFO "%s | read value: %08x\n", __func__, data_readed);
 	
-//Copia dei dati letti verso l'userspace
+/** Copia dei dati letti verso l'userspace */
+	
 	if (copy_to_user(buf, &data_readed, count))
 		return -EFAULT;
 
@@ -370,10 +375,10 @@ static ssize_t GPIO_write (struct file *file_ptr, const char *buf, size_t size, 
 	GPIO_dev_ptr = file_ptr->private_data;
 	if (*off > GPIO_dev_ptr->res_size)
 		return -EFAULT;
-//Copia dei dati dall'userspace
+/** Copia dei dati dall'userspace */
 	if (copy_from_user(&data_to_write, buf, size))
 		return -EFAULT;
-//Accesso ai registri del device
+/** Accesso ai registri del device */
 	write_addr = GPIO_GetDeviceAddress(GPIO_dev_ptr)+*off;
 	iowrite32(data_to_write, write_addr);
 	return size;
--- Michele/FPGA/UART/Driver/Con_interrupt/KERNEL_MODE/UART.c
+++ Michele/FPGA/UART/Driver/Con_interrupt/KERNEL_MODE/UART.c
@@ -8,7 +8,7 @@
 MODULE_LICENSE("GPL");
 
 /**
- * @brief Inizializza una struttura UART per il corrispondente device
+ * @brief Inizializza una struttura UART per il corrispondente device.
  *
  * @param 	UART_device puntatore a struttura UART, corrispondente al device su cui operare
  * @param 	owner puntatore a struttura struct module, proprietario del device (THIS_MODULE)
@@ -43,66 +43,70 @@ int UART_Init(		UART* UART_device,
 	UART_device->pdev = pdev;
 	UART_device->class = class;
 		
-	//genera Mj e min number
+/** Alloca un range di Mj e min numbers per il device a caratteri */
+	
 	if ((error = alloc_chrdev_region(&UART_device->Mm, 0 , 1, file_name)) != 0) {
 		printk(KERN_ERR "%s: alloc_chrdev_region() ha restituito %d\n", __func__, error);
 		return error;
 	}
 	
-	//registra il file operation sul device a caratteri
+/** Inizializza la struttura cdev specificando la struttura file operations associata al device a caratteri */
+	
 	cdev_init (&UART_device->cdev, f_ops);
 	UART_device->cdev.owner = owner;
 	
-	//Crea il device all'interno del filesystem assegnandogli il Mj e ne restituisce il puntatore.
+/** Crea il device all'interno del filesystem assegnandogli i numbers richiesti in precedenza e ne restituisce il puntatore. */
+	
 	if ((UART_device->dev = device_create(class, NULL, UART_device->Mm, NULL, file_name)) == NULL) {
 		printk(KERN_ERR "%s: device_create() ha restituito NULL\n", __func__);
 		error = -ENOMEM;
 		goto device_create_error;
 	}
 	
-	//fa uscire il device sotto /dev/
+/** Aggiunge il device a caratteri al sistema. Se l'operazione va a buon fine sarà possibile vedere il device sotto /dev */
+	
 	if ((error = cdev_add(&UART_device->cdev, UART_device->Mm, 1)) != 0) {
 		printk(KERN_ERR "%s: cdev_add() ha restituito %d\n", __func__, error);
 		goto cdev_add_error;
 	}
 	
-	//inizializza la risorsa con il valori presi dal device tree andando a recuperare gli indirizzi fisici del nodo
-	//parametro of_node è il device tree associato al nodo
+/** Inizializza la struct resource con il valori recuperati dal device tree corrispondente al device */
 	dev = &pdev->dev;
 	if ((error = of_address_to_resource(dev->of_node, 0, &UART_device->res)) != 0) {
 		printk(KERN_ERR "%s: address_to_resource() ha restituito %d\n", __func__, error);
 		goto of_address_to_resource_error;
 	}
+	
 	UART_device->res_size = UART_device->res.end - UART_device->res.start + 1;
 	
-	//alloco una quantita res_size di memoria fisica per il dispositivo IO a partire dall'inidirzzo res.start e resituisce l'inidirizzo
+/** Alloca una quantita res_size di memoria fisica per il dispositivo IO a partire dall'inidirzzo res.start e ne resituisce l'inidirizzo */
+	
 	if ((UART_device->mreg = request_mem_region(UART_device->res.start, UART_device->res_size, file_name)) == NULL) {
 		printk(KERN_ERR "%s: request_mem_region() ha restituito NULL\n", __func__);
 		error = -ENOMEM;
 		goto request_mem_region_error;
 	}
 	
-	//mappa la memoria fisca allocata e mi restituisce l'inidizzo virtuale
+/** Mappa la memoria fisca allocata e restituisce l'indirizzo virtuale */
+	
 	if ((UART_device->vrtl_addr = ioremap(UART_device->res.start, UART_device->res_size))==NULL) {
 		printk(KERN_ERR "%s: ioremap() ha restituito NULL\n", __func__);
 		error = -ENOMEM;
 		goto ioremap_error;
 	}
 	
+/** Cerca le specifiche dell'interrupt nel device tree e restituisce il suo numero identificativo */
 	
-	//mappa e si fa resituire il numero
 	UART_device->irqNumber = irq_of_parse_and_map(dev->of_node, 0);
-	
 	
 	if ((error = request_irq(UART_device->irqNumber , irq_handler, 0, file_name, NULL)) != 0) {
 		printk(KERN_ERR "%s: request_irq() ha restituito %d\n", __func__, error);
 		goto irq_of_parse_and_map_error;
 	}
-	UART_device->irq_mask = irq_mask;
-
 	
-		
-/** Inizializzazione della wait-queue per la system-call read() e poll() */
+	UART_device->irq_mask = irq_mask;
+	
+/** Inizializzazione della wait-queue per la system-call read(), poll() e write() */
 	
 	init_waitqueue_head(&UART_device->write_queue);
  	init_waitqueue_head(&UART_device->read_queue);
@@ -119,7 +123,6 @@ int UART_Init(		UART* UART_device,
 /** Abilitazione degli interrupt del device */
 	
  	UART_GlobalInterruptEnable(UART_device);
-	
 	UART_InterruptEnable(UART_device, UART_device->irq_mask);
 	
 	goto no_error;
@@ -138,14 +141,14 @@ int UART_Init(		UART* UART_device,
 	
 no_error:
 	
-	printk(KERN_INFO " IRQ registered as %d\n", UART_device->irqNumber);	
-	printk(KERN_INFO " Driver succesfully probed at Virtual Address 0x%08lx\n", (unsigned long) UART_device->vrtl_addr);
+	printk(KERN_INFO "IRQ registered as %d\n", UART_device->irqNumber);	
+	printk(KERN_INFO "Driver succesfully probed at Virtual Address 0x%08lx\n", (unsigned long) UART_device->vrtl_addr);
 	
 	return error;
-}EXPORT_SYMBOL(UART_Init);
+}
 
 /**
- * @brief Rimuove un device UART con le relative strutture kernel allocate per il suo funzionamento
+ * @brief Rimuove un device UART con le relative strutture kernel allocate per il suo funzionamento.
  *
  * @param device puntatore a struttura UART che indica l'istanza UART da rimuovere
  */
@@ -161,71 +164,75 @@ void UART_Destroy(UART* device) {
 	cdev_del(&device->cdev);
 	unregister_chrdev_region(device->Mm, 1);
 	
-}EXPORT_SYMBOL(UART_Destroy);
+}
 
 /**
-* @brief Utilizzata per asserire il flag "can_read" di uno specifico device UART
-*
-* @param device puntatore a struttura UART, device su cui operare
-*/
-
+ * @brief Utilizzata per asserire il flag "can_read" di uno specifico device UART.
+ *
+ * @param device puntatore a struttura UART, device su cui operare
+ */
 void UART_SetCanRead(UART* device) {
 	unsigned long flags;
 	spin_lock_irqsave(&device->slock_int, flags);
 	device-> can_read = 1;
 	spin_unlock_irqrestore(&device->slock_int, flags);
-}EXPORT_SYMBOL(UART_SetCanRead);
+}
 
-
+/**
+ * @brief Utilizzata per asserire il flag "can_write" di uno specifico device UART.
+ *
+ * @param device puntatore a struttura UART, device su cui operare
+ */
 void UART_SetCanWrite(UART* device) {
 	unsigned long flags;
 	spin_lock_irqsave(&device->write_lock, flags);
 	device-> can_write = 1;
 	spin_unlock_irqrestore(&device->write_lock, flags);
-}EXPORT_SYMBOL(UART_SetCanWrite);
-
+}
 
 /**
- * @brief Utilizzata per resettare il flag "can_read" di uno specifico device UART
+ * @brief Utilizzata per resettare il flag "can_read" di uno specifico device UART.
  *
  * @param device puntatore a struttura UART, che si riferisce al device su cui operare
  */
- 
 void UART_ResetCanRead(UART* device) {
 	unsigned long flags;
 	spin_lock_irqsave(&device->slock_int, flags);
 	device-> can_read = 0;
 	spin_unlock_irqrestore(&device->slock_int, flags);
-}EXPORT_SYMBOL(UART_ResetCanRead);
+}
 
-
-
+/**
+ * @brief Utilizzata per resettare il flag "can_write" di uno specifico device UART.
+ *
+ * @param device puntatore a struttura UART, che si riferisce al device su cui operare
+ */
 void UART_ResetCanWrite(UART* device) {
 	unsigned long flags;
 	spin_lock_irqsave(&device->write_lock, flags);
 	device-> can_write = 0;
 	spin_unlock_irqrestore(&device->write_lock, flags);
-}EXPORT_SYMBOL(UART_ResetCanWrite);
-
-
-
+}
 
 /**
  * @brief Testa il valore del flag "can_read". Se è uguale a 0, ovvero non è possibile effettuare
- * una lettura, mette in sleep il processo
+ * 		  una lettura, mette in sleep il processo.
  *
  * @param device puntatore a struttura UART, che si riferisce al device su cui operare
  */
-
 void UART_TestCanReadAndSleep(UART* device) {
 	wait_event_interruptible(device->read_queue, (device->can_read != 0));
-}EXPORT_SYMBOL(UART_TestCanReadAndSleep);
+}
 
-
+/**
+ * @brief Testa il valore del flag "can_write". Se è uguale a 0, ovvero non è possibile effettuare
+ * 		  una lettura, mette in sleep il processo.
+ *
+ * @param device puntatore a struttura UART, che si riferisce al device su cui operare
+ */
 void UART_TestCanWriteAndSleep(UART* device) {
 	wait_event_interruptible(device->write_queue, (device->can_write != 0));
-}EXPORT_SYMBOL(UART_TestCanWriteAndSleep);
-
+}
 
 /**
  * @brief Verifica che le operazioni di lettura risultino non-bloccanti.
@@ -250,52 +257,55 @@ unsigned UART_GetPollMask(UART *device, struct file *file_ptr, struct poll_table
 	spin_unlock(&device->slock_int);
 	printk(KERN_INFO "Poll, ritorna mask = %d", mask);
 	return mask;
-}EXPORT_SYMBOL(UART_GetPollMask);
+}
 
 /**
  * @brief Risveglia i processi in attesa sulle code di read e poll.
  *
  * @param device puntatore a struttura UART, che si riferisce al device su cui operare
  */
- 
 void UART_ReadPollWakeUp(UART* device) {
 	wake_up_interruptible(&device->read_queue);
 	wake_up_interruptible(&device->poll_queue);
-}EXPORT_SYMBOL(UART_ReadPollWakeUp);
-
-void UART_WriteWakeUp(UART* device) {
-	wake_up_interruptible(&device->read_queue);
-	wake_up_interruptible(&device->poll_queue);
-}EXPORT_SYMBOL(UART_WriteWakeUp);
-
-
+}
 
 /**
- * @brief Restituisce l'indirizzo virtuale di memoria cui è mappato un device
+ * @brief Risveglia i processi in attesa sulla coda di write.
  *
  * @param device puntatore a struttura UART, che si riferisce al device su cui operare
  */
-extern void* UART_GetDeviceAddress(UART* device) {
-	return device->vrtl_addr;
-}EXPORT_SYMBOL(UART_GetDeviceAddress);
+void UART_WriteWakeUp(UART* device) {
+	wake_up_interruptible(&device->write_queue);
+}
+
+
 
 /**
- * @brief Abilitazione interrupt globali;
+ * @brief Restituisce l'indirizzo virtuale di memoria cui è mappato un device.
+ *
+ * @param device puntatore a struttura UART, che si riferisce al device su cui operare
+ */
+void* UART_GetDeviceAddress(UART* device) {
+	return device->vrtl_addr;
+}
+
+/**
+ * @brief Abilitazione interrupt globali.
  *
  * @param device puntatore a struttura UART, che si riferisce al device su cui operare
  */
 void UART_GlobalInterruptEnable(UART* device) {
 	iowrite32(1, (device->vrtl_addr + GLOBAL_INTR_EN));
-}EXPORT_SYMBOL(UART_GlobalInterruptEnable);
+}
 
 /**
- * @brief Disabilitazione interrupt globali;
+ * @brief Disabilitazione interrupt globali.
  *
  * @param device puntatore a struttura UART, che si riferisce al device su cui operare
  */
 void UART_GlobalInterruptDisable(UART* device) {
 	iowrite32(0, (device->vrtl_addr + GLOBAL_INTR_EN));
-}EXPORT_SYMBOL(UART_GlobalInterruptDisable);
+}
 
 /**
  * @brief Abilitazione interrupt per i singoli pin del device.
@@ -307,7 +317,7 @@ void UART_InterruptEnable(UART* device, unsigned mask) {
 	unsigned reg_value = ioread32((device->vrtl_addr + INTR_EN));
 	reg_value |= mask;
 	iowrite32(reg_value, (device->vrtl_addr + INTR_EN));
-}EXPORT_SYMBOL(UART_InterruptEnable);
+}
 
 /**
  * @brief Disabilitazione interrupt per i singoli pin del device.
@@ -320,25 +330,23 @@ void UART_InterruptDisable(UART* device, unsigned mask) {
 	unsigned reg_value = ioread32((device->vrtl_addr + INTR_EN));
 	reg_value &= ~mask;
 	iowrite32(reg_value, (device->vrtl_addr + INTR_EN));
-}EXPORT_SYMBOL(UART_InterruptDisable);
+}
 
 /**
  * @brief Fornisce una maschera che indica quali interrupt non sono ancora stati serviti
  *		  e che quindi risultano pending.
  * @param device puntatore a struttura UART, che si riferisce al device su cui operare
  *
- * @return maschera riportante i pin per i quali gli interrupt non sono stati ancora serviti
+ * @return maschera riportante gli interrupt che non sono stati ancora serviti
  */
 unsigned UART_PendingInterrupt(UART* device) {
 	return ioread32((device->vrtl_addr + INTR_ACK_PEND));
-}EXPORT_SYMBOL(UART_PendingInterrupt);
+}
 
 /**
- * @brief Invia al device notifica di servizio di un interrupt;
+ * @brief Invia al device notifica di servizio dell'interrupt relativa alla trasmissione.
  *
  * @param device puntatore a struttura UART, che si riferisce al device su cui operare
- *
- * @param mask maschera di selezione degli interrupt da notificare
  */
 void UART_TXInterruptAck(UART* device) {
 	uint32_t mask = 0x00000001;
@@ -346,26 +354,45 @@ void UART_TXInterruptAck(UART* device) {
 	iowrite32(0, (device->vrtl_addr + INTR_ACK_PEND));
 }
 
+/**
+ * @brief Invia al device notifica di servizio dell'interrupt relativa alla ricezione.
+ *
+ * @param device puntatore a struttura UART, che si riferisce al device su cui operare
+ */
 void UART_RXInterruptAck(UART* device) {
 	uint32_t mask = 0x00000002;
 	iowrite32(mask, (device->vrtl_addr + INTR_ACK_PEND));
 	iowrite32(0, (device->vrtl_addr + INTR_ACK_PEND));
 }
 
+/**
+ * @brief Inserisce all'interno del registro DATA_IN del dispositivo UART specificato tramite
+ *        il parametro device il valore indicato nel parametro dataToSend.
+ *
+ * @param device puntatore a struttura UART, che si riferisce al device su cui operare
+ * @param dataToSend valore da inserire all'interno del registro
+ */
 void UART_SetData(UART* device, uint8_t dataToSend){
-	
 	iowrite32(dataToSend, (device->vrtl_addr + DATA_IN));
-
 }
 
-uint8_t UART_GetData(UART* device){
-	
+/**
+ * @brief Restituisce il valore contenuto nel registro RX_REG del dispositivo UART specificato.
+ *	      dal parametro device
+ *
+ * @param device puntatore a struttura UART, che si riferisce al device su cui operare
+ *
+ * @return valore contenuto nel registro ricezione del device
+ */
+uint8_t UART_GetData(UART* device){	
 	return ioread32((device->vrtl_addr + RX_REG));
-
 }
 
+/**
+ * @brief Asserisce il segnale TX_EN iniziando la trasmissione.
+ *
+ * @param device puntatore a struttura UART, che si riferisce al device su cui operare
+ */
 void UART_Start(UART* device){
-
 	iowrite32(0x00000001, (device->vrtl_addr + TX_EN));
-
 }
